2caedece5d1c476ef01c7ae446f18458704350e6
Alejandro Olivares Marín <elminster.aom@gmail.com>
2021-04-21 19:25:59 +0200

    Hack http server for generating random results

--- tests/http_server.py
+++ tests/http_server.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python3
 """
-Very simple HTTP server in python for logging requests
+Very simple HTTP server in python for logging requests (Code not cleaned up)
 See: mdonkers/server.py on https://gist.github.com/mdonkers
-Usage::
+Usage:
     ./server.py [<port>]
 """
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
+import random
+import time
 
 
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
-        self.send_response(200)
+        myResponse = random.choice([200, 302, 404, 500])
+
+        self.send_response(myResponse)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
     def do_GET(self):
-        logging.info(
-            "GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers)
-        )
+        random_delay = random.uniform(0, 15)
+        time.sleep(random_delay)
+        logging.info(f"GET_request - Path:{self.path}, Response_time:{random_delay}")
+
         self._set_response()
         self.wfile.write("GET request for {}".format(self.path).encode("utf-8"))
 
     def do_POST(self):
+        random_delay = random.uniform(0, 15)
+        time.sleep(random_delay)
         content_length = int(
             self.headers["Content-Length"]
         )  # <--- Gets the size of data
         post_data = self.rfile.read(content_length)  # <--- Gets the data itself
         logging.info(
-            "POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-            str(self.path),
-            str(self.headers),
-            post_data.decode("utf-8"),
+            f"POST_request - Path:{self.path}, Response_time:{random_delay}, Body:\n{post_data.decode('utf-8')}\n"
         )
 
         self._set_response()
@@ -57,4 +61,4 @@ if __name__ == "__main__":
     if len(argv) == 2:
         run(port=int(argv[1]))
     else:
-        run()
+        print(f"Usage:\n./server.py [<port>]")
