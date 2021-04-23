6f01dd3ccf0a98881e680ec0c62e29877b21f3b5
Alejandro Olivares Marín <elminster.aom@gmail.com>
2021-04-23 14:54:35 +0200

    First draft of data been stored in DB: Working

--- src/db_insert.py
+++ src/db_insert.py
@@ -2,37 +2,13 @@
 https://hakibenita.com/fast-load-data-python-postgresql#copy-data-from-a-string-iterator-with-buffer-size
 """
 
+import io
 import json
 import logging
-import mmap
 import psycopg2
 from psycopg2 import extras
 
 log = logging.getLogger("homeworks")
-
-
-class Text_mmap(mmap.mmap):
-    def __init__(self, fileno, length):
-        mmap.mmap.__init__(self)
-        self.mmap = mmap.mmap(fileno, length)
-
-    def read(self, reading_caracters=0) -> str:
-        # We asume UTF-8, therefore 4 bytes per caracter
-        bytes_data = self.mmap.read(reading_caracters * 4)
-        return bytes_data.decode
-
-    def readline(self) -> None:
-        bytes_data = self.mmap.readline()
-        return bytes_data.decode
-
-    def write(self, text_line) -> int:
-        print(text_line)
-        bytes_wroten = self.mmap.write(str.encode(text_line))
-        return bytes_wroten
-
-
-from typing import Iterator, Optional
-import io
 
 
 class Metrics_send:
@@ -52,33 +28,24 @@ class Metrics_send:
         if self.db_connect:
             self.db_connect.close()
 
-    def insert_metric_copy(self, metrics: list[str]) -> None:
-        with open(self.db_buffer_path, "w+b") as f:
-            # Open file for reading and writing in binary mode, truncating its content
-            f.truncate(40916)  # See https://stackoverflow.com/a/46883365
-            with Text_mmap(f.fileno(), 0) as mmap_buffer:
-                for metric_json in metrics:
-                    log.info(metric_json)
-                    metric = json.loads(metric_json)
-                    metric_str = (
-                        "|".join(
-                            map(
-                                str,
-                                (
-                                    metric["time"],
-                                    metric["web_url"],
-                                    metric["http_status"],
-                                    metric["resp_time"],
-                                    metric["regex_match"],
-                                ),
-                            )
-                        )
-                        + "\n"
-                    )
-                    # Indicate to DB that this is the end of data
-                    log.debug(f"metric_str: {metric_str}")
-                    # Encode metric to bynary (see: https://stackoverflow.com/a/17500651) and write to buffer
-                    mmap_buffer.write(metric_str)
+    @staticmethod
+    def clean_values(value) -> str:
+        if value:
+            return str(value)
+        else:
+            # Otherwise: psycopg2.errors.InvalidTextRepresentation: invalid input syntax for type boolean: "None"
+            return r"\N"
 
-                with self.db_connect.cursor() as db_cursor:
-                    db_cursor.copy_from(mmap_buffer, self.db_table, sep="|")
+    def insert_metric_copy(self, metrics: list[str]) -> None:
+        metrics_stringIO = io.StringIO()
+        for metric_json in metrics:
+            metric_dict = json.loads(metric_json)
+            metrics_stringIO.write(
+                "|".join(map(self.clean_values, metric_dict.values())) + "\n"
+            )
+        metrics_stringIO.seek(0)
+        log.debug(f"metric_str: {metrics_stringIO.read()}")
+        metrics_stringIO.seek(0)
+
+        with self.db_connect.cursor() as db_cursor:
+            db_cursor.copy_from(metrics_stringIO, self.db_table, sep="|")
